1ac3069cefed0e4c44fcbe2e3bcc718a5107184a
qismet <qismetqedirli5@gmail.com>
2021-09-02 20:56:26 +0400

    Initial commit

---
+++ src/main/java/az/crocusoft/jdbc/template/example/Book.java
@@ -0,0 +1,18 @@
+package az.crocusoft.jdbc.template.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Book {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+}
---
+++ src/main/java/az/crocusoft/jdbc/template/example/JdbcTemplateExampleApplication.java
@@ -0,0 +1,13 @@
+package az.crocusoft.jdbc.template.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcTemplateExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcTemplateExampleApplication.class, args);
+    }
+
+}
